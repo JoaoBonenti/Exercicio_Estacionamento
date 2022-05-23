8f3da091279695ca15ff530e9b29411738fddda4
Joao Guilherme Bonenti Nobrega <69513119+JoaoBonenti@users.noreply.github.com>
2022-05-23 16:29:53 -0300

    Update ExercicioEstacionamento.java

--- ExercicioEstacionamento.java
+++ ExercicioEstacionamento.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class ExercicioEstacionamento {
     public static void main(String[] args) {
+        //Uso do Scanner para entrada de dados
         try (Scanner scan = new Scanner(System.in)) {
+            //Entrada de dados da variável num 
             System.out.print("Digite '1' se voce for Idoso, '2' se voce for Deficiente, '3 se for Gestante ou '0' se nao for nenhum dos tres: ");
-
             int num = scan.nextInt(); 
 
+            //Condicional para determinar se é possível ou não estacionar
             if (num == 1) 
             { System.out.print("Ok, voce pode estacionar"); }
 
